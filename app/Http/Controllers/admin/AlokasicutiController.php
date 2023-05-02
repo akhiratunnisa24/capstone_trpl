b572d2f4ada6a764838cc02cad0753e0936017fd
yahya <yahya682@gmail.com>
2023-05-02 08:49:10 +0700

    update

--- app/Http/Controllers/admin/AlokasicutiController.php
+++ app/Http/Controllers/admin/AlokasicutiController.php
@@ -18,7 +18,7 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class AlokasicutiController extends Controller
 {
-    
+
     /**
      * Create a new controller instance.
      *
@@ -28,16 +28,21 @@ class AlokasicutiController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function index(Request $request)
     {
         $row = Karyawan::where('id', Auth::user()->id_pegawai)->first();
         $role = Auth::user()->role;
-        if ($role == 1 || $role ==2 && $row->jabatan == "Asisten Manajer") {
+        if ($role == 1 || $role == 2 && $row->jabatan == "Asisten Manajer") {
 
             //index
-            $alokasicuti = Alokasicuti::where('status', 1)->whereYear('sampai', '=', Carbon::now()->year)->get();
-            
+            $alokasicuti = Alokasicuti::join('settingalokasi', 'alokasicuti.id_settingalokasi', '=', 'settingalokasi.id')
+                ->join('jeniscuti', 'alokasicuti.id_jeniscuti', '=', 'jeniscuti.id')
+                ->join('departemen', 'alokasicuti.departemen', '=', 'departemen.id')
+                ->where('alokasicuti.status', 1)->whereYear('alokasicuti.sampai', '=', Carbon::now()->year)
+                ->select('alokasicuti.*', 'jeniscuti.jenis_cuti', 'departemen.nama_departemen')
+                ->get();
+
             //create
             $karyawan = Karyawan::all();
             // $jeniscuti = DB::table('settingalokasi')
@@ -48,9 +53,11 @@ class AlokasicutiController extends Controller
                 ->select('jeniscuti.*')
                 ->distinct()
                 ->get();
+            //$karyawan = Karyawan::join('departemen','karyawan.divisi', '=', 'departemen.id')
+            //->select('karyawan.*' , 'departemen.nama_departemen')
+            //->get();
 
-            return view('admin.alokasicuti.index', compact('jeniscuti','karyawan','alokasicuti','row'));
-
+            return view('admin.alokasicuti.index', compact('jeniscuti', 'karyawan', 'alokasicuti', 'row'));
         } else {
 
             return redirect()->back();
@@ -61,17 +68,16 @@ class AlokasicutiController extends Controller
     {
         try {
             $getTglmasuk = Karyawan::select('tglmasuk')
-            ->where('id','=',$request->id_karyawan)->first();
+                ->where('id', '=', $request->id_karyawan)->first();
 
             // dd($request->id_karyawan,$getTglmasuk);
-            if(!$getTglmasuk) {
+            if (!$getTglmasuk) {
                 throw new Exception('Data not found');
             }
-            return response()->json($getTglmasuk,200);
-
-        } catch (\Exception $e){
+            return response()->json($getTglmasuk, 200);
+        } catch (\Exception $e) {
             return response()->json([
-                'message' =>$e->getMessage()
+                'message' => $e->getMessage()
             ], 500);
         }
     }
@@ -79,40 +85,38 @@ class AlokasicutiController extends Controller
     public function getSettingalokasi(Request $request)
     {
         try {
-            $getSettingalokasi = Settingalokasi::select('id','id_jeniscuti','durasi','mode_alokasi','departemen','mode_karyawan')
-            ->where('id_jeniscuti','=',$request->id_jeniscuti)->first();
+            $getSettingalokasi = Settingalokasi::select('id', 'id_jeniscuti', 'durasi', 'mode_alokasi', 'departemen', 'mode_karyawan')
+                ->where('id_jeniscuti', '=', $request->id_jeniscuti)->first();
 
-            if(!$getSettingalokasi) {
+            if (!$getSettingalokasi) {
                 throw new \Exception('Data not found');
             }
-            return response()->json($getSettingalokasi,200);
-
-        } catch (\Exception $e){
+            return response()->json($getSettingalokasi, 200);
+        } catch (\Exception $e) {
             return response()->json([
-                'message' =>$e->getMessage()
+                'message' => $e->getMessage()
             ], 500);
         }
     }
 
     public function getAlokasicuti(Request $request)
-{
-    try {
-        $getAlokasicuti = Alokasicuti::select('*')
-        ->where('id_jeniscuti','=',$request->id_jeniscuti)
-        ->where('id_karyawan','=',$request->id_karyawan)
-        ->first();
+    {
+        try {
+            $getAlokasicuti = Alokasicuti::select('*')
+                ->where('id_jeniscuti', '=', $request->id_jeniscuti)
+                ->where('id_karyawan', '=', $request->id_karyawan)
+                ->first();
 
-        if(!$getAlokasicuti) {
-            throw new \Exception('Data not found');
+            if (!$getAlokasicuti) {
+                throw new \Exception('Data not found');
+            }
+            return response()->json($getAlokasicuti, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
         }
-        return response()->json($getAlokasicuti,200);
-
-    } catch (\Exception $e){
-        return response()->json([
-            'message' =>$e->getMessage()
-        ], 500);
-    } 
-}
+    }
     public function store(Request $request)
     {
         // dd($request->id_jeniscuti);
@@ -126,11 +130,10 @@ class AlokasicutiController extends Controller
         //     return redirect()->back()->with('danger', 'Data sudah ada');
         // }
 
-        if($request->id_jeniscuti == 1)
-        {
+        if ($request->id_jeniscuti == 1) {
             $validate = $request->validate([
                 'id_karyawan'  => 'required',
-                'id_settingalokasi'=> 'required',
+                'id_settingalokasi' => 'required',
                 'id_jeniscuti' => 'required',
                 'tgl_masuk'    => 'required',
                 'tgl_sekarang' => 'required',
@@ -138,9 +141,9 @@ class AlokasicutiController extends Controller
                 'sampai'       => 'required',
             ]);
 
-            $alokasicuti = New Alokasicuti;
+            $alokasicuti = new Alokasicuti;
             $alokasicuti->id_karyawan  = $request->id_karyawan;
-            $alokasicuti->id_settingalokasi= $request->id_settingalokasi;
+            $alokasicuti->id_settingalokasi = $request->id_settingalokasi;
             $alokasicuti->id_jeniscuti = $request->id_jeniscuti;
             $alokasicuti->durasi       = $request->durasi;
             $alokasicuti->mode_alokasi = $request->mode_alokasi;
@@ -151,20 +154,19 @@ class AlokasicutiController extends Controller
 
             // dd($alokasicuti);
             $alokasicuti->save();
-            return redirect()->back()->with('pesan','Data berhasil disimpan !');
-        }else
-        {
+            return redirect()->back()->with('pesan', 'Data berhasil disimpan !');
+        } else {
             $validate = $request->validate([
                 'id_karyawan'  => 'required',
-                'id_settingalokasi'=> 'required',
+                'id_settingalokasi' => 'required',
                 'id_jeniscuti' => 'required',
                 'aktif_dari'   => 'required',
                 'sampai'       => 'required',
             ]);
             // dd($validate);
-            $alokasicuti = New Alokasicuti;
+            $alokasicuti = new Alokasicuti;
             $alokasicuti->id_karyawan  = $request->id_karyawan;
-            $alokasicuti->id_settingalokasi= $request->id_settingalokasi;
+            $alokasicuti->id_settingalokasi = $request->id_settingalokasi;
             $alokasicuti->id_jeniscuti = $request->id_jeniscuti;
             $alokasicuti->durasi       = $request->durasi;
             $alokasicuti->mode_alokasi = $request->mode_alokasi;
@@ -176,7 +178,7 @@ class AlokasicutiController extends Controller
             // dd($alokasicuti);
             $alokasicuti->save();
             // return redirect()->back()->withInput();
-            return redirect()->back()->with('pesan','Data berhasil disimpan !');
+            return redirect()->back()->with('pesan', 'Data berhasil disimpan !');
         }
     }
 
@@ -184,7 +186,7 @@ class AlokasicutiController extends Controller
     {
         $row = Karyawan::where('id', Auth::user()->id_pegawai)->first();
         $alokasicuti = Alokasicuti::find($id);
-        return view('admin.alokasicuti.showalokasi',compact('alokasicuti','row'));
+        return view('admin.alokasicuti.showalokasi', compact('alokasicuti', 'row'));
     }
 
     //get data alokascuti
@@ -204,8 +206,7 @@ class AlokasicutiController extends Controller
     {
         $alokasicuti = Alokasicuti::find($id);
         dd($request->all());
-        if($request->id_jeniscuti == 1)
-        {
+        if ($request->id_jeniscuti == 1) {
             $validate = $request->validate([
                 'id_karyawan'  => 'required',
                 'id_jeniscuti' => 'required',
@@ -215,11 +216,11 @@ class AlokasicutiController extends Controller
                 'sampai'       => 'required',
             ]);
             $alokasicuti->update([
-                'id_karyawan'  =>$request->id_karyawan,
-                'id_settingalokasi'=> $request->id_settingalokasi,
-                'id_jeniscuti' =>$request->id_jeniscuti,
-                'durasi'       =>$request->durasi,
-                'mode_alokasi' =>$request->mode_alokasi,
+                'id_karyawan'  => $request->id_karyawan,
+                'id_settingalokasi' => $request->id_settingalokasi,
+                'id_jeniscuti' => $request->id_jeniscuti,
+                'durasi'       => $request->durasi,
+                'mode_alokasi' => $request->mode_alokasi,
                 // 'tgl_masuk'    => Carbon::parse($request->tgl_masuk)->format('Y-m-d'),
                 // 'tgl_sekarang' => Carbon::parse($request->tgl_sekarang)->format('Y-m-d'),
                 'aktif_dari'   => Carbon::parse($request->aktif_dari)->format('Y-m-d'),
@@ -229,11 +230,10 @@ class AlokasicutiController extends Controller
             return response()->json([
                 'success' => true,
                 'message' => 'Data Berhasil Diudapte!',
-                'statusCode'=>200,
+                'statusCode' => 200,
                 'data'    => $alokasicuti
             ]);
-        }else
-        {
+        } else {
             $validate = $request->validate([
                 'id_karyawan'  => 'required',
                 'id_jeniscuti' => 'required',
@@ -243,14 +243,14 @@ class AlokasicutiController extends Controller
 
             // dd($validate);
             $alokasicuti->update([
-                'id_karyawan'=>$request->id_karyawan,
-                'id_settingalokasi'=> $request->id_settingalokasi,
-                'id_jeniscuti'=>$request->id_jeniscuti,
-                'durasi'=>$request->durasi,
-                'mode_alokasi'=>$request->mode_alokasi,
-                'tgl_masuk' =>NULL,
-                'tgl_sekarang' =>NULL,
-                'aktif_dari'=> Carbon::parse($request->aktif_dari)->format('Y-m-d'),
+                'id_karyawan' => $request->id_karyawan,
+                'id_settingalokasi' => $request->id_settingalokasi,
+                'id_jeniscuti' => $request->id_jeniscuti,
+                'durasi' => $request->durasi,
+                'mode_alokasi' => $request->mode_alokasi,
+                'tgl_masuk' => NULL,
+                'tgl_sekarang' => NULL,
+                'aktif_dari' => Carbon::parse($request->aktif_dari)->format('Y-m-d'),
                 'sampai' => Carbon::parse($request->sampai)->format('Y-m-d'),
             ]);
 
@@ -258,7 +258,7 @@ class AlokasicutiController extends Controller
             return response()->json([
                 'success' => true,
                 'message' => 'Data Berhasil Diudapte!',
-                'statusCode'=>200,
+                'statusCode' => 200,
                 'data'    => $alokasicuti
             ]);
         }
@@ -282,7 +282,7 @@ class AlokasicutiController extends Controller
     {
         $row = Karyawan::where('id', Auth::user()->id_pegawai)->first();
         $alokasicuti = Alokasicuti::where('id_karyawan', Auth::user()->id_pegawai)->get();
-        return view('admin.alokasicuti', compact('alokasicuti','row'));
+        return view('admin.alokasicuti', compact('alokasicuti', 'row'));
     }
 }
 
