d88391ab9d7b8ff37e7ff6377d46af4d2a1d95ca
radika06 <Radzz900@gmail.com>
2023-08-02 14:36:05 +0700

    update user mesin done 2-8-2023

---
+++ app/Models/UserMesin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserMesin extends Model
+{
+    use HasFactory;
+    protected $table='user_mesin';
+    protected $fillable=['id_pegawai','nik','noid','departemen','partner']; 
+
+    public function departemens()
+    {
+        return $this->belongsTo(Departemen::class,'divisi','id');
+    }
+        
+    public function karyawan()
+    {
+        return $this->belongsTo(Karyawan::class, 'id_pegawai', 'id');
+    }
+}
