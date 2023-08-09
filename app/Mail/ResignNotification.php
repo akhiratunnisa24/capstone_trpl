3d213b4a1540000961dc73c8a3302f30291e631c
radika06 <Radzz900@gmail.com>
2023-08-09 17:20:34 +0700

    update resign notif diadmin  belum dicoba  9 agustus

---
+++ app/Mail/ResignNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ResignNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data=[];
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this->from('raddicacomp2@gmail.com','no-reply@grm.com')
+            ->subject($this->data['subject'])
+            ->cc($this->data['karyawan_email'], 'Karyawan')
+            ->cc(isset($this->data['atasan2']) ? $this->data['atasan2'] : '','Pimpinan Unit Kerja')
+            ->cc('akhiratunnisahasanah0917@gmail.com','HRD GRM')
+            ->view('emails.resignindex')->with('data',$this->data);
+
+            // ->cc('hrd-global@grm-risk.com','HRD GRM')
+            // ->cc('pandu@grm-risk.com','HRD Staff')
+            // ->cc('ariswan@grm-risk.com','HRD Manager')
+    }
+}
